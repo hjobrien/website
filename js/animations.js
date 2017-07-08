5494791579574cd626ce198a96b2a1731b43253b
Hank O'Brien <hank.obrien900@icloud.com>
2017-07-07 21:21:44 -0700

    made contant animations only occur once

--- js/animations.js
+++ js/animations.js
@@ -26,8 +26,6 @@ function check_if_in_view() {
             (element_top_position <= window_bottom_position)) {
             $element.addClass('in-view');
             console.log('element in view')
-        } else {
-            $element.removeClass('in-view');
         }
     });
 }
