d71f9c3fee95ea8ad36101f570322ca578a095cb
Hank O'Brien <hank.obrien900@icloud.com>
2017-07-10 00:35:33 -0700

    better overlapping box layout

--- js/skill_box_layouts.js
+++ js/skill_box_layouts.js
@@ -9,19 +9,14 @@ $window.on('scroll resize', change_box_layout);
 
 function change_box_layout() {
     var window_width = $window.width();
-    // console.log(window_width);
     $.each($skill_box, function () {
         var $element = $(this);
 
         if (window_width < 450) {
-            // $element.removeClass('nest-view');
             $element.addClass('vertical-view');
-            // console.log('switched views');
         }
         else {
             $element.removeClass('vertical-view');
-            // console.log('switched views');
-            // $element.addClass('nest-view');
         }
     });
 }
