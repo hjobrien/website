a3bfa43fbceb96f4b27b5b29eb79eee14ad0f1c8
Hank O'Brien <hank.obrien900@icloud.com>
2017-07-13 23:35:42 -0700

    added possible new paralax implementation

---
+++ js/parallax.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Hank on 7/13/17.
+ */
+
+const parallax_factor = 10
+
+const $window = $(window)
+const $background = $('.background')
+$window.on('scroll', scroll_background())
+
+function scroll_background () {
+    $.each($background, function () {
+        const $element = $(this)
+        var yPos = - ($element.scrollTop() / parallax_factor)
+
+        // Put together our final background position
+        var coords = '50% '+ yPos + 'px';
+
+        // Move the background
+        $bgobj.css({ backgroundPosition: coords });
+    })
+}
+
